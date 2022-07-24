5dd05a46aeeb38067e55768550498b6dbd4fd8f7
hatifmujahid <56687118+hatifmujahid@users.noreply.github.com>
2022-07-24 23:20:36 +0500

    first commit

---
+++ movie_app/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(ChatApp());
+}
+class ChatApp extends StatelessWidget {
+  const ChatApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
