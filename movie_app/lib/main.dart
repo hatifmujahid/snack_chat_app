906df367fa45436f4d7815eaed65590bc9236004
hatifmujahid <56687118+hatifmujahid@users.noreply.github.com>
2022-07-25 02:09:00 +0500

    Added a donut button and made a basic UI

--- movie_app/lib/main.dart
+++ movie_app/lib/main.dart
@@ -8,8 +8,83 @@ class ChatApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+              onPrimary: Colors.white,
+              primary: Color(0xFF212121),
+              minimumSize: Size(double.infinity, 50),
+              padding: EdgeInsets.all(8.0),
+              
+            ),
+          ),
+        ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Snack Chat App"),
+        ),
+        body: MainBodyHomePage(),
+      ),
     );
   }
 }
+
+class MainBodyHomePage extends StatefulWidget {
+  const MainBodyHomePage({ Key? key }) : super(key: key);
+
+  @override
+  State<MainBodyHomePage> createState() => _MainBodyHomePageState();
+}
+
+class _MainBodyHomePageState extends State<MainBodyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 8.0, bottom: 8.0),
+      width: 500,
+      child: Column(
+        children: <Widget>[
+          Donuts(),
+        ],
+      ),
+    );
+  }
+}
+Widget Donuts(){
+  return Container(
+    height: 70,
+    width: double.infinity,
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(100),
+        boxShadow: [
+          BoxShadow(
+            color: Color(0x80000000),
+            blurRadius: 12.0,
+            offset: Offset(0.0, 5.0),
+          ),
+        ],
+        gradient: const LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            Color.fromARGB(255, 231, 255, 51),
+            Color.fromARGB(255, 204, 153, 255),
+          ],
+        )
+      ),
+      child: Center(
+        child: Text(
+          "Donuts 🍩",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 20,
+          ),
+        ),
+      ),
+    );
+}
+
+ 
